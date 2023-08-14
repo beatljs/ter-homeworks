15e88460774c0794f6fd21848be0f33903497908
Serg Zhukov <beatljs@mail.ru>
2023-08-14 14:43:39 +0300

    hotfix after tflint and checkov

--- 04/src/variables.tf
+++ 04/src/variables.tf
@@ -14,12 +14,17 @@ variable "folder_id" {
   description = "https://cloud.yandex.ru/docs/resource-manager/operations/folder/get-id"
 }
 
+variable "vault_token" {
+  type        = string
+  description = "Vault token"
+}
+
 variable "default_zone" {
   type        = string
   default     = "ru-central1-a"
   description = "https://cloud.yandex.ru/docs/overview/concepts/geo-scope"
 }
-variable "default_cidr" {
+/*variable "default_cidr" {
   type        = list(string)
   default     = ["10.0.1.0/24"]
   description = "https://cloud.yandex.ru/docs/vpc/operations/subnet-create"
@@ -70,7 +75,7 @@ variable "pub_keys_fn" {
   type    = list
   default = [ "/home/beatl/.ssh/id_ed25519.pub", "/home/beatl/.ssh/id_rsa.pub" ]
 }
-
+*/
 
 
 
